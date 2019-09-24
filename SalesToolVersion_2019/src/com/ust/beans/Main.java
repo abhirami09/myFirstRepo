7f2380949e1836a8c17592754e472f52fdc1d9d8
dev12 <dev12@dev12-desktop>
2019-09-24 14:53:07 +0530

    initial commit by abhi

---
+++ SalesToolVersion_2019/src/com/ust/beans/Main.java
@@ -0,0 +1,13 @@
+package com.ust.beans;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesData salesData=new SalesData();
+		System.out.println("Welcome to gitHub World");
+		salesData.display();
+
+	}
+
+}
